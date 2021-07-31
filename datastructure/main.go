316126df2fd2ecb6845cb5ed4c7b9f4ebbcc6f21
Eakkawat Louhawattanavanich <eakkawatlouhawattanavanich@MacBook-Pro-khxng-Eakkawat.local>
2021-07-31 16:57:19 +0700

    add readme

--- datastructure/main.go
+++ datastructure/main.go
@@ -5,9 +5,25 @@ type Vertex struct {
 	X int
 	Y int
   }
-   
+  type Shape interface {
+	Area() float64
+	Perimeter() float64
+  }
+  type Rectangle struct {
+	Length, Width float64
+  }
   func main() {
 	v := Vertex{1, 2}
 	v.X = 4
 	fmt.Println(v.X, v.Y)
+	var r Shape = Rectangle{Length: 3, Width: 4}
+  fmt.Printf("Type of r: %T, Area: %v, Perimeter: %v.", r, r.Area(), r.Perimeter())
+  }
+
+  func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+  }
+  
+  func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Length + r.Width)
   }
